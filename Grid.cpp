83cb2989ce9d21bb3e0621ba50e3124e03b5c8aa
Baptiste Audugé <baptiste.auduge@gmail.com>
2023-11-06 22:01:27 +0100

    feat/fix: 📎line contains seq & utils Fixed `line_can_contain_sequence_gen`, in the case where there is no more sequences to add, the function should check if, the first elements are all white or empty. Added method `column_can_contain_sequence_gen` Added method `get_size` Added method `set_cell` Added method `get_cell` Added method `print_grid` Added attribute GridSize

--- Grid.cpp
+++ Grid.cpp
@@ -5,13 +5,16 @@
 #include "Grid.h"
 #include <vector>
 #include <cassert>
+#include <iostream>
+
 namespace tomographie {
 
     Grid::Grid (int n, int m, GridColors default_color) {
         grid = std::vector(n, std::vector(m, default_color));
+        size = std::make_pair(n, m);
     }
 
-    bool Grid::is_empty_or_match(int line, int i, int j, GridColors color) {
+    bool Grid::is_empty_or_match_line(int line, int i, int j, GridColors color) {
         assert(i <= j);
         for (; i < j; ++i) {
             if (grid[line][i] != EMPTY && grid[line][i] != color) return false;
@@ -19,7 +22,15 @@ namespace tomographie {
         return true;
     }
 
-     bool Grid::line_can_contain_sequence(int j, int l, std::vector<int> &seq) {
+    bool Grid::is_empty_or_match_col(int col, int i, int j, GridColors color) {
+        assert(i <= j);
+        for (; i < j; ++i) {
+            if (grid[i][col] != EMPTY && grid[i][col] != color) return false;
+        }
+        return true;
+    }
+
+     bool Grid::line_can_contain_sequence(int j, int l, Sequence &seq) {
          assert(l >= 0);
          if (!l) return true;
          if (j < seq[l-1] - 1) return false;
@@ -29,22 +40,72 @@ namespace tomographie {
      }
 
 
-    bool Grid::line_can_contain_sequence_gen(int line, int j, int l, std::vector<int> &seq) {
+    bool Grid::line_can_contain_sequence_gen(int line, int j, int l, Sequence &seq) {
         assert(l >= 0);
-        if (!l) return true;
+        if (!l) return is_empty_or_match_line(line, 0, j + 1, WHITE);
         if (j < seq[l-1] - 1) return false;
         if (j == seq[l-1] - 1) {
-            return l == 1 && is_empty_or_match(line, 0, j + 1, BLACK);
+            return l == 1 && is_empty_or_match_line(line, 0, j + 1, BLACK);
         }
         return (
-                (Grid::is_empty_or_match(line, j - seq[l-1] + 1, j + 1 , BLACK)
+                (is_empty_or_match_line(line, j - seq[l-1] + 1, j + 1 , BLACK)
                 // Checking if the separator (WHITE) is available
-                && Grid::is_empty_or_match(line, j - seq[l - 1], j - seq[l - 1] + 1, WHITE)
-                && Grid::line_can_contain_sequence_gen(line, j - seq[l - 1] - 1, l - 1, seq))
+                && is_empty_or_match_line(line, j - seq[l - 1], j - seq[l - 1] + 1, WHITE)
+                && line_can_contain_sequence_gen(line, j - seq[l - 1] - 1, l - 1, seq))
                 ||
-                (Grid::is_empty_or_match(line, j, j + 1, WHITE)
-                && Grid::line_can_contain_sequence_gen(line, j - 1, l, seq))
+                (is_empty_or_match_line(line, j, j + 1, WHITE)
+                && line_can_contain_sequence_gen(line, j - 1, l, seq))
                 );
     }
+
+    bool Grid::column_can_contain_sequence_gen(int column, int j, int l, Sequence &seq) {
+        assert(l >= 0);
+        if (!l) return is_empty_or_match_col(column, 0, j + 1, WHITE);
+        if (j < seq[l-1] - 1) return false;
+        if (j == seq[l-1] - 1) {
+            return l == 1 && is_empty_or_match_col(column, 0, j + 1, BLACK);
+        }
+        return (
+                (is_empty_or_match_col(column, j - seq[l-1] + 1, j + 1 , BLACK)
+                 // Checking if the separator (WHITE) is available
+                 && is_empty_or_match_col(column, j - seq[l - 1], j - seq[l - 1] + 1, WHITE)
+                 && column_can_contain_sequence_gen(column, j - seq[l - 1] - 1, l - 1, seq))
+                ||
+                (is_empty_or_match_col(column, j, j + 1, WHITE)
+                 && column_can_contain_sequence_gen(column, j - 1, l, seq))
+        );
+    }
+
+
+    GridSize Grid::get_size() {
+        return size;
+    }
+
+    void Grid::set_cell(int line, int col, GridColors value) {
+        assert(line < size.first && col < size.second);
+        grid[line][col] = value;
+    }
+
+    GridColors Grid::get_cell(int line, int col) {
+        return grid[line][col];
+    }
+
+
+    void Grid::print_grid() {
+        for (auto &line: grid) {
+            for (auto &cell: line) {
+                if (cell == EMPTY) {
+                    std::cout << std::endl << "Error: You are trying to print a Grid who's not full ..." << std::endl;
+                    return;
+                }
+                if (cell == BLACK)
+                    std::cout << "#";
+                if (cell == WHITE)
+                    std::cout << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
 
 } // tomographie
--- Grid.h
+++ Grid.h
@@ -7,6 +7,7 @@
 
 #include <string>
 #include <vector>
+#include "utils.h"
 
 namespace tomographie {
 
@@ -16,18 +17,34 @@ namespace tomographie {
         EMPTY
     };
 
+    typedef std::pair<int, int> GridSize;
+    typedef std::vector<GridColors> GridLine;
+
     class Grid {
         private:
+            std::vector<GridLine> grid;
+
             /**
-            * Check if the subsequence [i:j[ is empty or as the color `color`
+            * Check if the subsequence [i:j[ is empty or as the color `color` at the provided line.
             * @param line The line to test
             * @param i The first index to test
             * @param j The last index to last (! excluded)
             * @param color The color to check (or empty)
             * @return
             */
-            bool is_empty_or_match(int line, int i, int j, GridColors color);
-            std::vector<std::vector<GridColors>> grid;
+            bool is_empty_or_match_line(int line, int i, int j, GridColors color);
+
+            /**
+            * Check if the subsequence [i:j[ is empty or as the color `color` at the provided column.
+            * @param column The column to test
+            * @param i The first index to test
+            * @param j The last index to last (! excluded)
+            * @param color The color to check (or empty)
+            * @return
+            */
+            bool is_empty_or_match_col(int col, int i, int j, GridColors color);
+
+            GridSize size;
         public:
             /**
              * Constructor of the Grid
@@ -44,7 +61,7 @@ namespace tomographie {
             * @param seq The sequence
             * @return
             */
-            bool line_can_contain_sequence(int j, int l, std::vector<int> &seq);
+            bool line_can_contain_sequence(int j, int l, Sequence &seq);
 
             /**
             * Check if a line can contain a sequence, the function is taking under consideration
@@ -55,7 +72,47 @@ namespace tomographie {
             * @param seq The sequence
             * @return
             */
-            bool line_can_contain_sequence_gen(int line, int j, int l, std::vector<int> &seq);
+            bool line_can_contain_sequence_gen(int line, int j, int l, Sequence &seq);
+
+            /**
+             * Check if a  column can contain a sequence, the function is taking under consideration
+             * the current values of the column.
+             * @param column The column to test
+             * @param j The first cell of the column to check
+             * @param l The first index of the sequence
+             * @param seq The sequence
+             * @return
+             */
+            bool column_can_contain_sequence_gen(int column, int j, int l, Sequence & seq);
+
+            /**
+             * Returns the size of the Grid
+             * @return (num_line, num_col)
+             */
+            GridSize get_size();
+
+            /**
+             * Set the cell at [line][col] to the value `value`
+             * @param line The line to edit
+             * @param col The column to edit
+             * @param value The new value
+             */
+            void set_cell(int line, int col, GridColors value);
+
+            /**
+             * Returns the current value at [line][col]
+             * @param line The line to get the value from
+             * @param col The column to get the value from
+             * @return value at grid[line][col]
+             */
+            GridColors get_cell(int line, int col);
+
+            /**
+             * Function used to print a Grid, the function will
+             * print a message of error is the Grid is not full
+             * i.e. it contains EMPTY cells
+             */
+            void print_grid();
     };
 
 } // tomographie
